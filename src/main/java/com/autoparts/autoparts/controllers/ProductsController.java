cd1ecaf6a754a9691fdbe211a33f0868761b1bdc
Catherine <c.muthoni@alustudent.com>
2021-07-26 19:38:53 +0200

    Latest commit

--- src/main/java/com/autoparts/autoparts/controllers/ProductsController.java
+++ src/main/java/com/autoparts/autoparts/controllers/ProductsController.java
@@ -2,12 +2,12 @@
 package com.autoparts.autoparts.controllers;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-
+import java.util.NoSuchElementException;
 
 import javax.servlet.ServletContext;
-
 
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
@@ -18,10 +18,14 @@ import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.autoparts.autoparts.classes.OrderProduct;
 import com.autoparts.autoparts.classes.Products;
 import com.autoparts.autoparts.repository.ProductsRepository;
+import com.autoparts.autoparts.services.BusinessDetailsService;
 import com.autoparts.autoparts.services.ProductsService;
 
 import org.apache.commons.io.FilenameUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -33,9 +37,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
 
+@Configuration
+@PropertySource("classpath:application.properties")
 @Controller
 public class ProductsController {
 
@@ -48,10 +55,20 @@ public class ProductsController {
     @Autowired
     ServletContext servletContext;
 
+    @Value("${cloud.aws.credentials.accessKey}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secretKey}")
+    private String secretKey;
+
+    @Autowired
+    BusinessDetailsService businessDetailsService;
+
     // GET - all products- view all - products page
     @GetMapping("/shop")
     public String getAllProducts(Model model) {
         model.addAttribute("products", productsService.getAllProducts());
+        model.addAttribute("businessDetails", businessDetailsService.getOneDetail(0L));
         return "shop";
     }
 
@@ -59,15 +76,21 @@ public class ProductsController {
     @GetMapping(path = "/newproduct")
     public String showAddForm(Model model) {
         model.addAttribute("products", new Products());
+        model.addAttribute("businessDetails", businessDetailsService.getOneDetail(0L));
         return "addproduct";
     }
 
     // show more about product form
-    @GetMapping(path = "/{id}")
+    @GetMapping(path = "/product/{id}")
     public String showMore(@PathVariable("id") Long id, Model model) {
+        try {
+            model.addAttribute("products", productsService.getOneProduct(id));
+            model.addAttribute("orderProduct", new OrderProduct());
+            model.addAttribute("businessDetails", businessDetailsService.getOneDetail(0L));
+        } catch (NoSuchElementException e) {
+            return "error-404";
+        }
 
-        model.addAttribute("products", productsService.getOneProduct(id));
-        model.addAttribute("orderProduct", new OrderProduct());
         return "productview";
     }
 
@@ -76,9 +99,10 @@ public class ProductsController {
     public String saveProductSubmission(@ModelAttribute("products") @Valid Products product, Model model,
             BindingResult bindingResult, @RequestParam("studentPhoto") MultipartFile studentPhoto) throws IOException {
         if (bindingResult.hasErrors()) {
-            System.out.println("Error during add product!");
+            model.addAttribute("businessDetails", businessDetailsService.getOneDetail(0L));
             return "addproduct";
         }
+        model.addAttribute("businessDetails", businessDetailsService.getOneDetail(0L));
         productsService.addProduct(product);
         String extension = FilenameUtils.getExtension(studentPhoto.getOriginalFilename());
         String nameP = product.getProductId() + "." + extension;
@@ -86,16 +110,14 @@ public class ProductsController {
 
         productsService.addProduct(product);
 
-        // AWS S3
-        AWSCredentials credentials = new BasicAWSCredentials("AKIAXCMAX5TCHECNIJGH",
-                "fMcpM2NXnOZUi9dF/iWk4nmV9H0JP0kSk/q1xHDx");
+        AWSCredentials credentials = new BasicAWSCredentials(this.accessKey, this.secretKey);
         AmazonS3 s3client = AmazonS3ClientBuilder.standard()
                 .withCredentials(new AWSStaticCredentialsProvider(credentials)).withRegion(Regions.US_EAST_2).build();
 
         File file = convertMultiPartToFile(studentPhoto);
         s3client.putObject("autoparts250", nameP, file);
-
-        return "redirect:/shop";
+        model.addAttribute("success", "Product Added Successfully!");
+        return "addproduct";
     }
 
     private File convertMultiPartToFile(MultipartFile file) throws IOException {
@@ -108,10 +130,10 @@ public class ProductsController {
 
     // Show update form
     @GetMapping("/edit/{id}")
-    public ModelAndView showUpdateForm(@PathVariable("id") long id) {
+    public ModelAndView showUpdateForm(@PathVariable("id") long id, Model model) {
+        model.addAttribute("businessDetails", businessDetailsService.getOneDetail(0L));
         ModelAndView mav = new ModelAndView("updateproduct");
-
-        Products product = productsService.getOneProduct(id);        
+        Products product = productsService.getOneProduct(id);
         mav.addObject("product", product);
         return mav;
     }
@@ -119,46 +141,47 @@ public class ProductsController {
     // UPDATE A PRODUCT
     @RequestMapping(value = "/updateproduct", method = RequestMethod.POST)
     public String updateProductSubmission(@ModelAttribute("product") Products product, BindingResult bindingResult,
-            Model model, @RequestParam("studentPhoto") MultipartFile studentPhoto) throws IOException {
-
+            Model model, @RequestParam(value = "studentPhoto", required = false) MultipartFile studentPhoto)
+            throws IOException {
+        model.addAttribute("businessDetails", businessDetailsService.getOneDetail(0L));
         if (bindingResult.hasErrors()) {
+            model.addAttribute("businessDetails", businessDetailsService.getOneDetail(0L));
             return "updateproduct";
         }
         // photo
-        productsService.addProduct(product);
-        String extension = FilenameUtils.getExtension(studentPhoto.getOriginalFilename());
-        String nameP = product.getProductId() + "." + extension;
-        product.setPhoto(nameP);
+        try {
+            productsService.addProduct(product);
+            String extension = FilenameUtils.getExtension(studentPhoto.getOriginalFilename());
+            String nameP = product.getProductId() + "." + extension;
+            product.setPhoto(nameP);
 
-        // productsService.addProduct(product);
+            // AWS S3
+            AWSCredentials credentials = new BasicAWSCredentials(this.accessKey, this.secretKey);
 
-        // FileUploadUtil.saveFile("http://s3.amazonaws.com/autoparts250", nameP, studentPhoto);
+            AmazonS3 s3client = AmazonS3ClientBuilder.standard()
+                    .withCredentials(new AWSStaticCredentialsProvider(credentials)).withRegion(Regions.US_EAST_2)
+                    .build();
 
-        // AWS S3
-        AWSCredentials credentials = new BasicAWSCredentials("AKIAXCMAX5TCBFYGLRGH",
-                "CKbqWUr8iIhR/gorR7OYIKCgFWya/r1BDIa9UQV8");
-        AmazonS3 s3client = AmazonS3ClientBuilder.standard()
-                .withCredentials(new AWSStaticCredentialsProvider(credentials)).withRegion(Regions.US_EAST_2).build();
+            File file = convertMultiPartToFile(studentPhoto);
 
-        File file = convertMultiPartToFile(studentPhoto);
-        s3client.putObject("autoparts250", nameP, file);
+            s3client.putObject("autoparts250", nameP, file);
 
+            productsService.addProduct(product);
+            model.addAttribute("success", "Product updated Successfully!");
+            return "updateproduct";
+        } catch (FileNotFoundException e) {
+            productsService.addProduct(product);
+            model.addAttribute("success", "Product updated Successfully!");
+            return "updateproduct";
+        }
 
-        // photo
-
-        // get previous photo
-        // MultipartFile previousPhoto = productsService.getOneProduct(product.getProductId()).getStudentPhoto();
-
-        productsService.addProduct(product);
-
-        return "redirect:/shop";
     }
 
     // DELETE - delete a product
     @GetMapping("/delete/{productId}")
-    public String delProduct(@PathVariable("productId") Long productId) {
+    public String delProduct(@PathVariable("productId") Long productId, Model model, RedirectAttributes attributes) {
         productsService.delProduct(productId);
-
+        attributes.addFlashAttribute("success", "Product deleted Successfully!");
         return "redirect:/shop";
 
     }
